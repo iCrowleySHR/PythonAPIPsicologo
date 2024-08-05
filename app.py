0d3a10ab2c9b5271ab2d2fd2a5926766f825b7d1
Gustavo Gualda <104951600+iCrowleySHR@users.noreply.github.com>
2024-08-05 17:40:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -51,13 +51,13 @@ def search_psychologist():
         
         # Preenchendo e enviando os dados
         input_nome.send_keys(nome)
-        # btn_busca_avancada.click()
+        btn_busca_avancada.click()
         
-        # # Espera até que o campo de CPF esteja visível e disponível para inserção
-        # WebDriverWait(driver, 40).until(
-        #     EC.visibility_of(input_cpf)
-        # )
-        # input_cpf.send_keys(cpf)
+        # Espera até que o campo de CPF esteja visível e disponível para inserção
+        WebDriverWait(driver, 40).until(
+            EC.visibility_of(input_cpf)
+        )
+        input_cpf.send_keys(cpf)
         time.sleep(2)
         btn_buscar.click()
 
