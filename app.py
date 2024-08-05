6aaf0e422d85120a7b87271a180af367f110c450
Gustavo Gualda <104951600+iCrowleySHR@users.noreply.github.com>
2024-08-05 16:10:49 -0300

    feat

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+import time
 
 app = Flask(__name__)
-
-# Caminho para o ChromeDriver
-CHROMEDRIVER_PATH = 'C:\\Users\\Gustavo Gualda\\AppData\\Local\\Programs\\Python\\Python312\\chromedriver.exe'
 
 @app.route('/search_psychologist', methods=['POST'])
 def search_psychologist():
@@ -21,9 +21,8 @@ def search_psychologist():
         return jsonify({'error': 'Nome e CPF são obrigatórios.'}), 400
 
     # Configurando o WebDriver
-    service = Service(CHROMEDRIVER_PATH)
     options = Options()
-    driver = webdriver.Chrome(service=service, options=options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
     try:
         # Acessando o site
@@ -58,15 +57,33 @@ def search_psychologist():
         WebDriverWait(driver, 40).until(
             EC.visibility_of(input_cpf)
         )
-        # input_cpf.send_keys(cpf)
+        input_cpf.send_keys(cpf)
+        time.sleep(2)
         btn_buscar.click()
 
         # Espera até que o resultado esteja presente
-        resultado = WebDriverWait(driver, 550).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/article/div/div/div[2]/div/div/table/tbody'))  # Substitua pelo XPath real do resultado
-        ).text
+        resultado_html = WebDriverWait(driver, 550).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/article/div/div/div[2]/div/div/table'))
+        ).get_attribute('outerHTML')
 
-        return jsonify({'status': 'success', 'resultado': resultado})
+        # Usando BeautifulSoup para analisar o HTML
+        soup = BeautifulSoup(resultado_html, 'lxml')
+        tabela = soup.find('table')
+        linhas = tabela.find('tbody').find_all('tr')
+
+        resultados = []
+        for linha in linhas:
+            colunas = linha.find_all('td')
+            resultado = {
+                'Status': colunas[0].text.strip() if len(colunas) > 0 else '',
+                'Nome': colunas[1].text.strip() if len(colunas) > 1 else '',
+                'Região': colunas[2].text.strip() if len(colunas) > 2 else '',
+                'Número do Registro': colunas[3].text.strip() if len(colunas) > 3 else '',
+                'Data de Registro': colunas[4].text.strip() if len(colunas) > 4 else ''
+            }
+            resultados.append(resultado)
+
+        return jsonify({'status': 'success', 'resultados': resultados})
 
     except Exception as e:
         return jsonify({'status': 'error', 'message': str(e)}), 500
